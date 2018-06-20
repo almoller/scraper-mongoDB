2a3314cc72a8cd39c63206029950ffa0aa711ce9
almoller <moller.alan@gmail.com>
2018-06-20 02:23:54 -0600

    database connections for heroku

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ var cheerio = require("cheerio");
 // Require all models
 var db = require("./models");
 
-var PORT = 3000;
+var PORT = process.env.PORT || 3000;
 
 // Initialize Express
 var app = express();
@@ -27,7 +27,9 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
 // Connect to the Mongo DB
-mongoose.connect("mongodb://localhost/mongoScraperdb");
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoScraperdb";
+mongoose.Promise = Promise;
+mongoose.connect(MONGODB_URI);
 
 // Routes
 
